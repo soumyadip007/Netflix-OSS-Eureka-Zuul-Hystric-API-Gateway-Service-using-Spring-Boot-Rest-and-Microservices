0bdc4e3fd8050c11dd28f0915ac7036553ffd882
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-28 13:16:47 +0530

    Serverapp

---
+++ zuul-service/Server/server/HelloServerApplication.java
@@ -0,0 +1,16 @@
+package com.eureka.zuul.hystrix.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HelloServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloServerApplication.class, args);
+	}
+}
+
+
